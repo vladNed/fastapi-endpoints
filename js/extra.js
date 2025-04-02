1a3c0bcd721c76ac6d81bae865bb894015293845
Vlad Nedelcu <nedelcuvd@gmail.com>
2025-04-02 09:19:50 +0300

    Deployed 49cfa15 with MkDocs version: 1.6.0

--- js/extra.js
+++
@@ -1,4 +0,0 @@
-var meta = document.createElement("meta");
-meta.name = "google-site-verification";
-meta.content = "5E3YRkieGK79Sr5Ipwu0wStoGHMPMUssZ1HaMRZTpjo";
-document.getElementsByTagName("head")[0].appendChild(meta);
